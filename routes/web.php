351e5d31f8adf287a88f85e6003259301945b965
Victor <victor_brandao@outlook.com>
2022-03-20 13:25:33 -0300

    feat: agrupamento de rotas

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return 'kk eae men';
-// });
-
 Route::get('/', 'PrincipalController@principal');
-
 Route::get('/sobre', 'SobreNosController@sobreNos');
-
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get(
-    '/contato/{nome}/{categoria_id?}',
-    function (
-        string $nome,
-        int $categoria_id = 1
-    ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-)->where('categoria_id', '[0-9]+')
-    ->where('nome', '[A-Za-z]+');
+Route::get('/login', function() { return 'Login'; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; });
+    Route::get('/fornecedores', function() { return 'Fornecedores'; });
+    Route::get('/produtos', function() { return 'Produtos'; });
+});

facde47c080371992c29c6d1bf2d6eb707380dd8
Victor <victor_brandao@outlook.com>
2022-03-20 13:15:56 -0300

    feat: menu de anvegação

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -1,1 +1,15 @@
 <h3>Contato (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+
+    <li>
+        <a href="/sobre">Sobre Nós</a>
+    </li>
+
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,15 @@
 <h3>Sobre Nós (view)</h3>
+
+<ul>
+    <li>
+        <a href="/">Principal</a>
+    </li>
+
+    <li>
+        <a href="/sobre">Sobre Nós</a>
+    </li>
+
+    <li>
+        <a href="/contato">Contato</a>
+    </li>
+</ul>

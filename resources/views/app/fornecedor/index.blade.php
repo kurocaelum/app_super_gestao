5e022a9984cdb6c1a6c5061f5933243b267e561e
Victor <victor_brandao@outlook.com>
2022-04-26 18:11:25 -0300

    Blade - isset

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,21 @@
 <h3>Fornecedor</h3>
 
-Fornecedor: {{ $fornecedores[0]['nome'] }}
-<br />
-Status: {{ $fornecedores[0]['status'] }}
-<br /> <br />
+@isset($fornecedores)
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[0]['status'] }}
+    <br />
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+    @endisset
+    <br /><br />
 
-@unless ($fornecedores[0]['status'] == 'S')
-    Fornecedor inativo
-@endunless
+    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    <br />
+    Status: {{ $fornecedores[1]['status'] }}
+    <br />
+    @isset($fornecedores[1]['cnpj'])
+        CNPJ: {{ $fornecedores[1]['cnpj'] }}
+    @endisset
+    <br /><br />
+@endisset
